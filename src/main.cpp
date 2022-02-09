cc18c29b27a8fd9adc5aca3cf196d73f4deac31f
damik422 <damik422@gmail.com>
2022-02-09 21:06:13 +0100

    fixes

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@
 #define IN2 23
 
 //wartości dla inicjalizacji PWM
-const int freq = 10; //częstotliwość pwm
+const int freq = 20; //częstotliwość pwm
 const int channel = 0;  //kanał pwm
 const int resolution = 8; //rozdzielczość (0-255)
 //wartości sensora (potencjometru)
@@ -26,8 +26,8 @@ unsigned int dt = 0;  //różnica czasu
 unsigned long prevt = 0;  //poprzedni czas
 //wartości do pętli PID
 float kp = 1; //współczynnik KP
-float ki = 0.0001; //współczynnik KI
-float kd = 10; //współczynnik KD
+float ki = 0; //współczynnik KI
+float kd = 0; //współczynnik KD
 int inputPosition = 50;  //pozycja zadana
 int pwm = 0;  //wyjście do elementu sterującego silnikiem
 int error = 0;  //uchyb
@@ -75,9 +75,9 @@ void PID (int input) {
   pwm = kp*error + ki*totalError*dt + kd*(error-prevError)/dt; //PID (k + całka + różniczka)
   
   if (error > 0){ //zmiana kierunku silnika w zależności od tego w którą stronę jest uchyb
-    direction = 0;
-  } else {
     direction = 1;
+  } else {
+    direction = 0;
   }
 
   pwm = constrain(abs(pwm), 0, 255); // wyeliminowanie większych wartości niż jest w stanie podać pin (255 to wartość odpowiadająca 100% wypełnieniu sygnału) oraz wzięcie wartości bezwzględnej bo kierunek regulujemy przez direction
